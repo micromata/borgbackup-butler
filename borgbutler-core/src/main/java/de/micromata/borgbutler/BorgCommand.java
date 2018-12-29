503c8bdcd975032f9f315c1233e99c69d200a065
Kai Reinhard <K.Reinhard@micromata.de>
2018-12-29 10:21:29 +0100

    Job handling removed (done by JobQueue).

--- borgbutler-core/src/main/java/de/micromata/borgbutler/BorgCommand.java
+++ borgbutler-core/src/main/java/de/micromata/borgbutler/BorgCommand.java
@@ -16,28 +16,6 @@ import java.io.File;
 public class BorgCommand {
     private Logger log = LoggerFactory.getLogger(BorgCommand.class);
 
-    /**
-     * The current Thread assigned to this command sets the state and reads for the state {@link State#TERMINATE_SIGNAL_RECEIVED}.
-     * If received, the thread will be terminated without executing the borg command.
-     */
-    public enum State {
-        /**
-         * This command is waiting in the queue.
-         */
-        WAITING_IN_QUEUE,
-        /**
-         * This command is running (beeing executed).
-         */
-        RUNNING,
-        /**
-         * This command should not be terminated before starting.
-         */
-        TERMINATE_SIGNAL_RECEIVED}
-
-    public enum ResultStatus {
-        OK, ERROR
-    }
-
     @Getter
     @Setter
     private File workingDir;
@@ -54,8 +32,6 @@ public class BorgCommand {
     @Getter
     @Setter
     private String archive;
-    @Getter
-    private State state;
     /**
      * For displaying and information purposes for the user only, when browsing the current command queue.
      */
@@ -68,17 +44,6 @@ public class BorgCommand {
     @Getter
     @Setter(AccessLevel.PACKAGE)
     private String response;
-
-    @Getter
-    @Setter(AccessLevel.PACKAGE)
-    private ResultStatus resultStatus;
-
-    /**
-     * Requests the thread handling this command to terminate / cancel this action.
-     */
-    public void terminate() {
-        this.state = State.TERMINATE_SIGNAL_RECEIVED;
-    }
 
     BorgCommand setArgs(String... args) {
         this.args = args;
