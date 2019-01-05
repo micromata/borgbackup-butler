0d47869eec99a795c9cc1f927090d3d82eef37b5
Kai Reinhard <K.Reinhard@micromata.de>
2019-01-05 14:36:21 +0100

    Title...

--- borgbutler-webapp/src/components/general/modal/ConfirmModal.jsx
+++ borgbutler-webapp/src/components/general/modal/ConfirmModal.jsx
@@ -18,7 +18,7 @@ class ConfirmModal extends React.Component {
     render() {
         return (
             <Modal isOpen={this.props.open} toggle={this.props.toggle}>
-                <ModalHeader toggle={this.props.toggle}>Modal title</ModalHeader>
+                <ModalHeader toggle={this.props.toggle}>{this.props.title}</ModalHeader>
                 <ModalBody>
                     {this.props.children}
                 </ModalBody>
