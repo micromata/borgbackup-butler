4dce9eaac96829f08b153813c946bd9d7df2ff85
Kai Reinhard <K.Reinhard@micromata.de>
2019-02-10 16:16:29 +0100

    Button style info added.

--- borgbutler-webapp/src/components/general/forms/FormButton.jsx
+++ borgbutler-webapp/src/components/general/forms/FormButton.jsx
@@ -34,7 +34,7 @@ class FormButton extends React.Component {
 
 FormButton.propTypes = {
     id: PropTypes.string,
-    bsStyle: PropTypes.oneOf(['primary', 'outline-primary', null]),
+    bsStyle: PropTypes.oneOf(['primary', 'outline-primary', 'info', null]),
     type: PropTypes.string,
     onClick: PropTypes.func,
     hint: PropTypes.string,
