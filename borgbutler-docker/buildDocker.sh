76527724d1a8981e733468aa3d338e5630efb6c5
Kai Reinhard <K.Reinhard@micromata.de>
2022-02-19 08:48:49 +0100

    Docker: kreinhard -> micromata

--- borgbutler-docker/buildDocker.sh
+++ borgbutler-docker/buildDocker.sh
@@ -9,12 +9,12 @@ mkdir -p app/target/dependency && (cd app/target/dependency; unzip ../../../../b
 # Will be done by application after starting: (cd app; wget https://github.com/borgbackup/borg/releases/download/1.1.16/borg-linux64.tgz)
 
 echo "Building docker file..."
-(cd app; docker build -t kreinhard/borgbutler .)
+(cd app; docker build -t micromata/borgbutler .)
 
 echo "docker tag ..... version"
-echo "docker push kreinhard/borgbutler:version"
-echo "docker push kreinhard/borgbutler:latest"
+echo "docker push micromata/borgbutler:version"
+echo "docker push micromata/borgbutler:latest"
 echo
 echo
-echo "Run 'docker run -v $HOME/BorgButler:/BorgButler -p 127.0.0.1:9042:9042 --name borgbutler kreinhard/borgbutler'"
+echo "Run 'docker run -v $HOME/BorgButler:/BorgButler -p 127.0.0.1:9042:9042 --name borgbutler micromata/borgbutler'"
 echo "For remote ssh repos, use option  '-v $HOME/.ssh:/home/borgbutler/.ssh:ro'"
